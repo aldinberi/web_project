ad5cd6ca9e5e0fe7df3127868074071bf17a9972
aldin <aldinberisa1514@gmail.com>
2019-12-27 13:14:56 +0100

    Fixing bug in deployment for sure this time

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,11 +1,11 @@
 
-// let baseUrl;
-// if (!process.env.HEROKU) {
-//     baseUrl = 'http://localhost:3001/'
-// } else {
-//     baseUrl = ''
-// }
+let baseUrl;
+if (!process.env.HEROKU) {
+    baseUrl = 'http://localhost:3001/'
+} else {
+    baseUrl = 'https://gran-app-react.herokuapp.com/'
+}
 
 module.exports = {
-    BASE_URL: 'http://localhost:3001/'
+    BASE_URL: baseUrl
 }
